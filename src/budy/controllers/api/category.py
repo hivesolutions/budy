1d8e80134aa2fdd81adc0a5fb54b89a776d8986d
joamag <joamag@gmail.com>
2015-04-14 14:23:22 +0100

    new category controller

--- src/budy/controllers/api/category.py
+++ src/budy/controllers/api/category.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Budy. If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,14 +37,14 @@ __copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
-from . import base
-from . import brand
-from . import category
-from . import media
-from . import product
+import appier
 
-from .base import BaseApiController
-from .brand import BrandApiController
-from .category import CategoryApiController
-from .media import MediaApiController
-from .product import ProductApiController
+import budy
+
+class CategoryApiController(appier.Controller):
+
+    @appier.route("/api/category", "GET", json = True)
+    def list_json(self):
+        object = appier.get_object(alias = True, find = True)
+        categories = budy.Category.find(map = True, **object)
+        return categories
