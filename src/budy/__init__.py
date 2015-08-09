e12733c38b94a0245645eb525f53ccdc5ad8b824
joamag <joamag@gmail.com>
2015-08-09 11:16:41 +0100

    small code fix

--- src/budy/__init__.py
+++ src/budy/__init__.py
@@ -34,10 +34,6 @@ __copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
 __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
-from . import controllers
-from . import models
 from . import main
 
-from .controllers import *
-from .models import *
 from .main import BudyApp
