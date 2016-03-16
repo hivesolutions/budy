47fdaebc88456e8e2bae59d6aa13513fbbb7a25a
joamag <joamag@gmail.com>
2016-03-16 08:03:53 +0000

    new models added

--- src/budy/models/checkout_line.py
+++ src/budy/models/checkout_line.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the Apache License along with
 # Hive Budy. If not, see <http://www.apache.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,36 +37,12 @@ __copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
 __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
-from . import account
-from . import bag_line
-from . import bag
-from . import base
-from . import brand
-from . import category
-from . import checkout_line
-from . import checkout
-from . import color
-from . import composition
-from . import live_model
-from . import measurement
-from . import media
-from . import product
-from . import season
-from . import subscription
+import appier
 
-from .account import BudyAccount
-from .bag_line import BagLine
-from .bag import Bag
-from .base import BudyBase
-from .brand import Brand
-from .category import Category
-from .checkout_line import CheckoutLine
-from .checkout import Checkout
-from .color import Color
-from .composition import Composition
-from .live_model import LiveModel
-from .measurement import Measurement
-from .media import Media
-from .product import Product
-from .season import Season
-from .subscription import Subscription
+from . import base
+
+class CheckoutLine(base.BudyBase):
+
+    quantity = appier.field(
+        type = float
+    )
