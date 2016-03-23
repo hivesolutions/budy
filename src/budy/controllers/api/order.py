046a25f70c581f4b9307abeaace8ecdce40b985e
joamag <joamag@hive.pt>
2016-03-23 14:18:09 +0000

    new empty bag

--- src/budy/controllers/api/order.py
+++ src/budy/controllers/api/order.py
@@ -141,8 +141,11 @@ class OrderApiController(root.RootApiController):
     @appier.ensure(token = "user")
     def pay(self, key):
         data = appier.request_json()
+        empty_bag = self.field("empty_bag", True, cast = bool)
         order = budy.Order.get(key = key, rules = False)
         order.pay_s(data)
+        bag = budy.Bag.from_session()
+        if empty_bag and bag: bag.empty_bag_s()
         order = order.reload(
             eager = (
                 "lines",
