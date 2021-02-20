7bb3c040e8fc1db6b33537cfc63bc2d02484b9ab
João Magalhães <joamag@gmail.com>
2021-02-20 19:46:52 +0000

    version: 0.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "budy",
-    version = "0.1.8",
+    version = "0.2.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Budy E-commerce System",
