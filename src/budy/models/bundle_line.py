cdc071056228f49fbe019d2b94d95411577fe818
João Magalhães <joamag@gmail.com>
2021-04-10 11:00:54 +0100

    docs: more comments

--- src/budy/models/bundle_line.py
+++ src/budy/models/bundle_line.py
@@ -110,7 +110,11 @@ class BundleLine(base.BudyBase):
         longer be re-calculated as the line is now frozen"""
     )
 
-    attributes = appier.field()
+    attributes = appier.field(
+        observations = """Additional metadata that define the given
+        (bundle) line, this is typically used to store a JSON serialized
+        string with extra fields"""
+    )
 
     product = appier.field(
         type = appier.reference(
