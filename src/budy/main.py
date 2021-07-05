306752083259b3a08c3d9a5dd6b8257fb5c7cd8a
João Magalhães <joamag@gmail.com>
2021-07-05 19:51:50 +0100

    version: 0.6.1

--- src/budy/main.py
+++ src/budy/main.py
@@ -83,7 +83,7 @@ class BudyApp(appier.WebApp):
         return self.omni_api
 
     def _version(self):
-        return "0.6.0"
+        return "0.6.1"
 
     def _description(self):
         return "Budy"
