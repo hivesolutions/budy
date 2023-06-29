951b61c7efc97d880a679774172f9b4fbf7e84f6
João Magalhães <joamag@gmail.com>
2023-06-29 12:14:09 +0100

    version: 0.8.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [0.8.5] - 2023-06-29
+
+### Fixed
+
+* Bumped `appier` version
+
 ## [0.8.4] - 2023-06-01
 
 ### Added
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "budy",
-    version = "0.8.4",
+    version = "0.8.5",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Budy E-commerce System",
