2ccf524f65268ff1047c482781022c096bb6279b
João Magalhães <joamag@gmail.com>
2025-01-03 18:49:19 +0000

    version: 0.8.14

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [0.8.14] - 2025-01-03
+
+### Fixed
+
+* Added more customer info to MBWay
+
 ## [0.8.13] - 2025-01-03
 
 ### Fixed
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import setuptools
 
 setuptools.setup(
     name="budy",
-    version="0.8.13",
+    version="0.8.14",
     author="Hive Solutions Lda.",
     author_email="development@hive.pt",
     description="Budy E-commerce System",
