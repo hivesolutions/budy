453923e3d5a36f404c69d1a1ec52b34f61d9979c
João Magalhães <joamag@gmail.com>
2025-02-18 16:01:31 +0000

    fix: version number

--- src/budy/main.py
+++ src/budy/main.py
@@ -85,7 +85,7 @@ class BudyApp(appier.WebApp):
         return self.seeplus_api
 
     def _version(self):
-        return "0.8.18"
+        return "0.8.19"
 
     def _description(self):
         return "Budy"
