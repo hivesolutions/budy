c89b80c49240c67d5f5d44c8a2c7f1581683db18
João Magalhães <joamag@gmail.com>
2025-06-17 11:08:26 +0100

    chore: new meta values

--- src/budy/controllers/api/voucher.py
+++ src/budy/controllers/api/voucher.py
@@ -92,17 +92,17 @@ class VoucherAPIController(root.RootAPIController):
         usage_limit = object.get("usage_limit", 0)
         start = object.get("start", None)
         expiration = object.get("expiration", None)
-        meta = object.get("meta", {})
         unlimited = object.get("unlimited", None)
+        meta = object.get("meta", {})
         key = self.field("key", key, cast=str)
         percentage = self.field("percentage", percentage, cast=float)
         usage_limit = self.field("usage_limit", usage_limit, cast=int)
         start = self.field("start", start, cast=int)
         expiration = self.field("expiration", expiration, cast=int)
-        meta = self.field("meta", meta, cast=dict)
         unlimited = self.field("unlimited", unlimited, cast=bool)
+        meta = self.field("meta", meta, cast=dict)
         voucher = budy.Voucher.create_percentage_s(
-            key, percentage, usage_limit, start, expiration, meta
+            key, percentage, usage_limit, start, expiration, unlimited, meta
         )
         voucher = voucher.map()
         return voucher
